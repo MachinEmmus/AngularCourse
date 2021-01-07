9bb0e3a989fbbc06abdab9b130d1ade4b94b6a0d
MachinEmmus <emonsalvep38@gmail.com>
2021-01-07 16:06:28 -0500

    Add because change my HDD by SSD

---
+++ AngularCrud/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFirebaseDatabaseModule } from '@angular-devkit/build-angular';
+import { environment } from 'src/environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirebaseDatabaseModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
